ef0e2197878283c4316e81b969ffa809f891b5a0
Wang Yabin <386231725@qq.com>
2018-04-13 23:19:23 +0800

    add feed

--- Groups/generatefeedvector.py
+++ Groups/generatefeedvector.py
@@ -1,5 +1,6 @@
 import feedparser
 import re
+import sys
 
 def getWordCounts(url):
     d=feedparser.parse(url)
@@ -16,3 +17,41 @@ def getWordCounts(url):
         wc.setdefault(word,0)
         wc[word]+=1
     return d.feed.title,wc
+
+def getwords(html):
+    txt=re.compile(r'<[^>]+>').sub('',html)
+    words=re.compile(r'[^A-Z^a-z]+').split(txt)
+    return [word.lower() for word in words if word!='']
+
+feedset=open('/home/wang/PycharmProjects/collective_intelligence/Groups/feedlist.txt','r')
+apcount={}
+wordcounts={}
+feedlist=[line for line in feedset.readlines()]
+for feedurl in feedlist:
+  try:
+    title,wc=getWordCounts(feedurl)
+    wordcounts[title]=wc
+    for word,count in wc.items():
+      apcount.setdefault(word,0)
+      if count>1:
+        apcount[word]+=1
+  except:
+    print('Failed to parse feed %s' % feedurl)
+
+wordlist=[]
+for w,bc in apcount.items():
+  frac=float(bc)/len(feedlist)
+  if frac>0.1 and frac<0.5:
+    wordlist.append(w)
+
+out=open('/home/wang/PycharmProjects/collective_intelligence/Groups/blogdata1.txt','w')
+out.write('Blog')
+for word in wordlist: out.write('\t%s' % word)
+out.write('\n')
+for blog,wc in wordcounts.items():
+  print(blog)
+  out.write(blog)
+  for word in wordlist:
+    if word in wc: out.write('\t%d' % wc[word])
+    else: out.write('\t0')
+  out.write('\n')
