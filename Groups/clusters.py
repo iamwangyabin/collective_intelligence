e3ed813affff6333f06568ec1b7d0c8f0e5de0a7
wangyabin <yabinwango@gmail.com>
2018-04-15 18:02:57 +0800

    add k-means algorithm

--- Groups/clusters.py
+++ Groups/clusters.py
@@ -56,7 +56,7 @@ def hcluster(rows, distance=pearson):
             for j in range(i + 1, len(clust)):
                 if (clust[i].id, clust[j].id) not in distances:
                     distances[(clust[i].id, clust[j].id)] = distance(clust[i].vec, clust[j].vec)
-                    
+
                 d = distances[(clust[i].id, clust[j].id)]
                 if d < closest:
                     closest = d
@@ -71,22 +71,24 @@ def hcluster(rows, distance=pearson):
         clust.append(newcluster)
     return clust[0]
 
-def printClust(clust,labels=None,n=0):
+
+def printClust(clust, labels=None, n=0):
     for i in range(n):
-        print(' ',)
-    if clust.id<0:
+        print(' ', )
+    if clust.id < 0:
         print('-')
     else:
-        if labels==None:
+        if labels == None:
             print(clust.id)
         else:
             print(labels[clust.id])
-    if clust.left!=None:
-        printClust(clust.left,labels=labels,n=n+1)
-    if clust.right!=None:
-        printClust(clust.right,labels=labels,n=n+1)
+    if clust.left != None:
+        printClust(clust.left, labels=labels, n=n + 1)
+    if clust.right != None:
+        printClust(clust.right, labels=labels, n=n + 1)
 
-from PIL import Image,ImageDraw
+
+from PIL import Image, ImageDraw
 
 
 def getheight(clust):
@@ -174,8 +176,7 @@ def kcluster(rows, distance=pearson, k=4):
 
     lastmatches = None
     for t in range(100):
-        print
-        'Iteration %d' % t
+        print('Iteration %d' % t)
         bestmatches = [[] for i in range(k)]
 
         # Find which centroid is the closest for each row
@@ -252,8 +253,7 @@ def scaledown(data, distance=pearson, rate=0.01):
 
                 # Keep track of the total error
                 totalerror += abs(errorterm)
-        print
-        totalerror
+        print(totalerror)
 
         # If the answer got worse by moving the points, we are done
         if lasterror and lasterror < totalerror: break
@@ -275,3 +275,43 @@ def draw2d(data, labels, jpeg='mds2d.jpg'):
         y = (data[i][1] + 0.5) * 1000
         draw.text((x, y), labels[i], (0, 0, 0))
     img.save(jpeg, 'JPEG')
+
+
+import random
+
+
+def kcluster(rows, distance=pearson, k=4):
+    ranges = [(min([row[i] for row in rows]), max([row[i] for row in rows])) for i in range(len(rows[0]))]
+
+    clusters = [[random.random() * (ranges[i][1] - ranges[i][0]) + ranges[i][0]
+    for i in range(len(rows[0]))] for j in range(k)]
+
+    lastmatches = None
+
+    for t in range(100):
+        print('Iteration %d' % t)
+        bestmatches = [[] for i in range(k)]
+
+        for j in range(len(rows)):
+            row = rows[j]
+            bestmatche = 0
+            for i in range(k):
+                d = distance(clusters[i], row)
+                if d < distance(clusters[bestmatche], row):
+                    bestmatche = i
+            bestmatches[bestmatche].append(j)
+
+        if bestmatches == lastmatches:
+            break
+        lastmatches = bestmatches
+
+        for i in range(k):
+            avgs = [0.0] * len(rows[0])
+            if len(bestmatches[i]) > 0:
+                for rowid in bestmatches[i]:
+                    for m in range(len(rows[rowid])):
+                        avgs[m] += rows[rowid][m]
+                for j in range(len(avgs)):
+                    avgs[j] /= len(bestmatches[i])
+                clusters[i] = avgs
+    return bestmatches
