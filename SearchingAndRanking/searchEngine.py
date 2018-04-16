b4f80ed4fb89a30c1eafc02b3ceed3e0427f96e5
wangyabin <yabinwango@gmail.com>
2018-04-16 18:12:05 +0800

    add database support --sqlite

--- SearchingAndRanking/searchEngine.py
+++ SearchingAndRanking/searchEngine.py
@@ -1,20 +1,25 @@
 import urllib
 from bs4 import BeautifulSoup
+import urllib.request
+import urllib.parse
+import sqlite3
 
 class crawler:
     def __init__(self,dbname):
-        pass
+        self.con=sqlite3.connect(dbname)
 
     def __del__(self):
-        pass
+        self.con.close()
+
     def dbcommit(self):
-        pass
+        self.con.commit()
 
     def getEntryId(self,table,field,value,createnew=True):
         return None
 
     def addToIndex(self,url,soup):
-        print('Indexing %s' % url)
+        if self.isIndexed(url):
+            return
 
     def getTextOnly(self,soup):
         return None
@@ -28,31 +33,47 @@ class crawler:
 
     def crawl(self, pages, depth=2):
         for i in range(depth):
-            newpages = set()
+            newpages = {}
             for page in pages:
                 try:
                     c = urllib.request.urlopen(page)
                 except:
                     print('Could not open %s' % page)
                     continue
-                soup = BeautifulSoup(c.read())
-                self.addToIndex(page, soup)
-                links = soup('a')
-                for link in links:
-                    if ('href' in dict(link.attrs)):
-                        url = urllib.parse.urljoin(page, link['href'])
-                        if url.find("'") != -1:
-                            continue
-                        url = url.split('#')[0]
-                        if url[0:4] == 'https' and not self.isIndexed(url):
-                            newpages.add(url)
-                        linkText = self.getTextOnly(link)
-                        self.addLinkRef(page, url, linkText)
-                self.dbcommit()
+                try:
+                    soup = BeautifulSoup(c.read())
+                    self.addToIndex(page, soup)
+
+                    links = soup('a')
+                    for link in links:
+                        if ('href' in dict(link.attrs)):
+                            url = urllib.parse.urljoin(page, link['href'])
+                            if url.find("'") != -1:
+                                continue
+                            url = url.split('#')[0]
+                            print(url)
+                            if url[0:4] == 'http' and not self.isIndexed(url):
+                                newpages[url]=1
+                            linkText = self.getTextOnly(link)
+                            self.addLinkRef(page, url, linkText)
+                    self.dbcommit()
+                except:
+                    print('Coule not parse page %s' % page)
             pages = newpages
+            print(newpages)
 
     def createIndexTables(self):
-        pass
+        self.con.execute('create table urllist(url)')
+        self.con.execute('create table wordlist(word)')
+        self.con.execute('create table wordlocation(urlid,wordid,location)')
+        self.con.execute('create table link(fromid integer,toid integer)')
+        self.con.execute('create table linkwords(wordid,linkid)')
+        self.con.execute('create index wordidx on wordlist(word)')
+        self.con.execute('create index urlidx on urllist(url)')
+        self.con.execute('create index wordurlidx on wordlocation(wordid)')
+        self.con.execute('create index urltoidx on link(toid)')
+        self.con.execute('create index urlfromidx on link(fromid)')
+        self.dbcommit()
 
 ignoreWords=set(['the','of','to','and','a','in','is','it'])
 
